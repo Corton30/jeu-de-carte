31fbd17a8c12b2ff86edea827151a768edd8d861
aligabr03 <aligabr03@gmail.com>
2023-12-13 16:47:52 -0500

    Cleaned up repo

---
+++ SiteWeb/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <h1>saba7 el fol</h1>
+    );
+};
+
+export default App;
